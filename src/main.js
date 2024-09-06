a85314eaecefc6a776253af3358dbf0ae2d25e0d
fexell <fexellofficial@gmail.com>
2024-09-06 14:34:30 +0200

    Added FontAwesome (free).

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
 import App from './App.vue'
 import router from './router'
 
 import './assets/tw.css'
 import './assets/main.sass'
 
+library.add(fab)
+library.add(far)
+library.add(fas)
+
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
