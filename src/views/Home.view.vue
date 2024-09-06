1bf669ce98c17dbe2b4ffd95c0b1aae5c4172413
fexell <fexellofficial@gmail.com>
2024-09-06 14:08:40 +0200

    Added flex to most 'containers.'

--- src/views/Home.view.vue
+++ src/views/Home.view.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-  <section id='Home' class='home view'>
-    <div class='home-container view-container'>
+  <section id='Home' class='home view flex flex-col'>
+    <div class='home-container view-container flex flex-col'>
       
     </div>
   </section>
